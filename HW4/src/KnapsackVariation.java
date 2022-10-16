c563d19bcdaa5c1c814927d1361ec2aa436929a4
Keenan Miller <43387387+keenanm500@users.noreply.github.com>
2022-10-16 12:19:14 -0400

    commit at least normal knapsack sorta works

--- HW4/src/KnapsackVariation.java
+++ HW4/src/KnapsackVariation.java
@@ -14,11 +14,11 @@ public class KnapsackVariation {
         int[] val = new int[n + 1];
         wt[0] = 0;
         val[0] = 0;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             wt[i] = scanner.nextInt();
             val[i] = scanner.nextInt();
         }
-        
+        // wt = mergeSort(wt, val);
         System.out.println(knapsackDP(w, W, wt, val, n));
     }
 
@@ -27,34 +27,38 @@ public class KnapsackVariation {
             return 0;
         }
 
-        Knapsack[][] OPT = new Knapsack[n][W2 + 1];
-        for(int i = 0; i < n; i++) {
+        Knapsack[][] OPT = new Knapsack[n + 1][W2 + 1];
+        for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= W2; j++) {
                 OPT[i][j] = new Knapsack(0, 0);
             }
         }
-
-        for (int i = 1; i <= W2; i++) {
-            for (int j = 1; j < n; j++) {
-                OPT[j][i] = OPT[j-1][i];
-                if(w[j] <= i && OPT[j-1][i-w[j]].value + v[j] >= OPT[j][i].value) {
-                    OPT[j][i] = new Knapsack(OPT[j-1][i-w[j]],  v[j], w[j]);
+        for (int j = 1; j < n + 1; j++) {
+            for (int i = 1; i <= W2; i++) {
+                OPT[j][i] = OPT[j - 1][i];
+                if (w[j] <= i && OPT[j - 1][i - w[j]].value + v[j] > OPT[j][i].value) {
+                    OPT[j][i] = new Knapsack(OPT[j - 1][i - w[j]], v[j], w[j]);
                 }
+
             }
         }
         System.out.println();
         for (int r = 0; r < OPT.length; r++) {       //for loop for row iteration.
             for (int c = 0; c < OPT[r].length; c++) {   //for loop for column iteration.
-                System.out.print(OPT[r][c].totalWeight + " " + OPT[r][c].value + " ");
+                if (OPT[r][c] != null) {
+                    System.out.print(OPT[r][c].totalWeight + "-" + OPT[r][c].value + " ");
+                } else {
+                    System.out.print("N N ");
+                }
             }
             System.out.println(); //using this for new line to print array in matrix format.
         }
-    
+
         // get max knapsack with min weight
         int result = 0;
         for (int i = 0; i < OPT.length; i++) {
             for (int j = 0; j < OPT[i].length; j++) {
-                if(OPT[i][j] != null && OPT[i][j].value > result && OPT[i][j].totalWeight >= W1) {
+                if (OPT[i][j] != null && OPT[i][j].value > result && OPT[i][j].totalWeight >= W1) {
                     result = OPT[i][j].value;
                 }
             }
@@ -63,7 +67,6 @@ public class KnapsackVariation {
 
     }
 }
-
 class Knapsack {
     int value = 0;
     int totalWeight = 0;
