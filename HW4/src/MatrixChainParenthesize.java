26746ca5c97107be9c0d4927bcebc43394d5dcd1
keenanmiller <keenan@marginedge.com>
2022-10-20 10:25:09 -0400

    matrix chain complete

--- HW4/src/MatrixChainParenthesize.java
+++ HW4/src/MatrixChainParenthesize.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 class MatrixChainParenthesize {
 
+    static int[][] traceBack;
     static int getOptMatrixChain(int[] matrixArray) {
 
         int[][] OPT = new int[matrixArray.length][matrixArray.length];
+        traceBack = new int[matrixArray.length][matrixArray.length];
         for (int d = 0; d < OPT.length; d++) {
             for (int i = 1; i < OPT.length - d; i++) {
                 int j = i + d;
@@ -17,11 +19,24 @@ class MatrixChainParenthesize {
                     int tmp = OPT[i][k] + OPT[k + 1][j] + matrixArray[i - 1] * matrixArray[k] * matrixArray[j];
                     if (OPT[i][j] > tmp) {
                         OPT[i][j] = tmp;
+                        traceBack[i][j] = k;
                     } 
                 }
             }
         }
         return OPT[1][OPT.length - 1];
+    }
+    
+    private static String writeSolution(int l, int r) {
+        if(l == r) {
+            return "A" + l;
+        }
+        if(l + 1 == r) {
+            // base case
+            return "( A" + l + " x A" + r + " )";
+        }
+        
+        return "( " + writeSolution(l, traceBack[l][r]) + " x " + writeSolution(traceBack[l][r] + 1, r) + " )";
     }
 
     public static void main (String[] args) {
@@ -34,5 +49,6 @@ class MatrixChainParenthesize {
         }
         
         System.out.println(getOptMatrixChain(matrices));
+        System.out.println(writeSolution(1, n));
     }
 }
